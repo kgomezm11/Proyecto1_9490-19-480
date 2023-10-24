4e61ac7d22c9114829b7912f9968911b5cee0fc0
Kelvin J. Gómez <kelvinjgomez2000@hotmail.com>
2023-10-24 17:13:34 -0600

    Update

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -30,7 +30,7 @@ router.post('/login', async (req, res) => {
 // POST - SE CREA UN NUEVO USUARIO SEGÚN LOS DATOS INGRESADOS
 router.post('/registro/:dpi', async (req, res) => {
     try {
-        const regex = /^[A-Za-z]+$/;
+        const regex = /^[A-Za-z\s]+$/;
         const dpi = req.params.dpi;
         const {
             nombres,
@@ -118,7 +118,7 @@ router.get('/perfil/:dpi', verifyToken, (req, res) => {
 
 //PATCH - MODIFICA LOS DATOS ASOCIADOS AL USUARIO
 router.patch('/perfil/:dpi', verifyToken, async (req, res) => {
-    const regex = /^[A-Za-z]+$/;
+    const regex = /^[A-Za-z\s]+$/;
     const dpiBuscar = req.params.dpi;
     if (dpiBuscar == req.dpi) {
 
